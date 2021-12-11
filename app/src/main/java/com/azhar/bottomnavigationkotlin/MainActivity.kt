2a857cc5ce4ce33caa6b7afea1e829321bb0dece
MD. Azharul Islam <36479711+azharcse14@users.noreply.github.com>
2021-12-11 21:44:39 +0600

    update

--- app/src/main/java/com/azhar/bottomnavigationkotlin/MainActivity.kt
+++ app/src/main/java/com/azhar/bottomnavigationkotlin/MainActivity.kt
@@ -2,10 +2,32 @@ package com.azhar.bottomnavigationkotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.azhar.bottomnavigationkotlin.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setFragment(VideosFragment())
+        binding.bottomNav.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.video_view_id->setFragment(VideosFragment())
+                R.id.folder_id->setFragment(FoldersFragment())
+            }
+            return@setOnItemSelectedListener true
+        }
+
     }
+
+    private fun setFragment(fragment: Fragment){
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.fragment_fl, fragment)
+        transaction.disallowAddToBackStack()
+        transaction.commit()
+    }
+
 }
